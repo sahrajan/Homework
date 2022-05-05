e07c0c8bd82f0dfa35f0f9bfaaf8151c0020d71c
Rajan Sah <83794911+sahrajan@users.noreply.github.com>
2022-05-05 14:34:11 -0400

    updated

--- homework2/src/Album.java
+++ homework2/src/Album.java
@@ -1,6 +1,8 @@
 /**
+ * 
  * @author Rajan Sah
- *
+ * @version 2.0
+ * @Date 05/05/2022
  */
 public class Album {
 	
@@ -12,6 +14,12 @@ public class Album {
 	
 	public static int numAlbums;
 	
+	/**
+	 * 
+	 * @param title
+	 * @param favoriteTrack
+	 * @param trackNumber
+	 */
 	public Album(String title, Song favoriteTrack, int trackNumber) {
 		// TODO Auto-generated constructor stub
 		
@@ -25,40 +33,76 @@ public class Album {
 		
 	}
 	
+	/**
+	 * 
+	 * @param title
+	 * @param favoriteTrack
+	 */
+	
 	public Album(String title, Song favoriteTrack) {
 		trackNumber = 1;
 		return;
 	}
 
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getTitle() {
 		return title;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getGenre() {
 		return genre;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public Song getFavoriteTrack() {
 		return favoriteTrack;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public int getTrackNumber() {
 		return trackNumber;
 	}
 
+	/**
+	 * 
+	 * @param favoriteTrack
+	 */
 	public void setFavoriteTrack(Song favoriteTrack) {
 		this.favoriteTrack = favoriteTrack;
 	}
 	
+	/**
+	 * 
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 	
+	/**
+	 * 
+	 * @param genre
+	 */
 	public void setGenre(String genre) {
 		this.genre = genre;
 	}
 
+	/**
+	 * 
+	 */
 	public String toString() {
 		return title+ "," +artist+ ","+genre ;
 		
